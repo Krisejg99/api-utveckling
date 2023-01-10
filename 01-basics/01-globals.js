1f1ad011a547ac0fadb5802851afb517ebebebf5
Kristopher Gray <kristopher.gray99@gmail.com>
2023-01-10 11:44:28 +0100

    init commit

---
+++ 01-basics/01-globals.js
@@ -0,0 +1,11 @@
+/**
+ * Globals
+ */
+
+// console.log('YO YO!')
+
+// setTimeout(() => console.log("Hi, sorry I'm late..."), 3000)
+// setInterval(() => console.log('Hello'), 2000)
+
+console.log(__filename)
+console.log(__dirname)
