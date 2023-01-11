248f6ad89838a353c7b6b0748fef7c4878152e7c
Kristopher Gray <kristopher.gray99@gmail.com>
2023-01-11 15:41:13 +0100

    oneliners random joke sent as response with GET at /joke

--- 02-express/server.js
+++ 02-express/server.js
@@ -3,8 +3,8 @@
  */
 
 const express = require('express')
+const { on } = require('nodemon')
 const PORT = 3000
-
 const app = express()
 
 app.get('/', (req, res) => {
@@ -22,3 +22,29 @@ app.get('/coffee', (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Server started on localhost:${PORT}`)
 })
+
+
+
+/**
+ * Workshop start
+ */
+
+const shuffleArray = (array) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
+
+const oneliners = require('./data/oneliners.json')
+
+app.get('/joke', (req, res) => {
+	shuffleArray(oneliners)
+	res.json({ joke: oneliners[0] })
+})
+
+/**
+ * Workshop end
+ */
