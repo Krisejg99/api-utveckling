fe1f0c71e8e4bd431766fa2f99998f0692f71d37
Kristopher Gray <kristopher.gray99@gmail.com>
2023-01-12 14:16:58 +0100

    morgan and lodash added. users.json with GET requests for each userId. Started POST request to users.

--- 02-express/server.js
+++ 02-express/server.js
@@ -3,9 +3,18 @@
  */
 
 const express = require('express')
+const _ = require('lodash')
+const morgan = require('morgan')
 const { on } = require('nodemon')
+
+const oneliners = require('./data/oneliners.json')
+const users = require('./data/users.json')
+
 const PORT = 3000
 const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
 	res.send({ message: "What's up bitch!" })
@@ -16,35 +25,44 @@ app.post('/', (req, res) => {
 })
 
 app.get('/coffee', (req, res) => {
-	res.send("Coffee is nasty, precious, but we needs it, yes we needs it to survive, precious, yeees...")
+	res.send("Coffee is nasty, precious, but we needs it, we needs it to survive, precious, yeees...")
+})
+
+app.get('/joke', (req, res) => {
+	const joke = _.sample(oneliners)
+	res.send({
+		joke: joke
+	})
+})
+
+app.get('/users', (req, res) => {
+	res.send(users)
+})
+
+app.get('/users/:userId', (req, res) => {
+	const user = users.find(user => user.id === Number(req.params.userId))
+	if (user) {
+		res.send(user)
+	}
+	else {
+		res.status(404).send({
+			message: `Sorry, no user with id '${req.params.userId}' exists.`
+		})
+	}
+
+})
+
+app.post('/users', (req, res) => {
+	console.log(req.body)
+	res.send(req.body)
+})
+
+app.use((req, res) => {
+	res.status(404).send({
+		message: `Sorry, no route exists for ${req.method} ${req.path}`
+	})
 })
 
 app.listen(PORT, () => {
 	console.log(`Server started on localhost:${PORT}`)
 })
-
-
-
-/**
- * Workshop start
- */
-
-const shuffleArray = (array) => {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		const temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-}
-
-const oneliners = require('./data/oneliners.json')
-
-app.get('/joke', (req, res) => {
-	shuffleArray(oneliners)
-	res.json({ joke: oneliners[0] })
-})
-
-/**
- * Workshop end
- */
