0750a3794594e57d6102d5fee9e9650f92477585
Kristopher Gray <kristopher.gray99@gmail.com>
2023-01-17 14:54:06 +0100

    functions made for app.get(/url) and app.get(/url/:id). added .env to gitignore

--- 03-mysql/server.js
+++ 03-mysql/server.js
@@ -3,10 +3,23 @@
  */
 
 // Require stuff
+require('dotenv').config()
 const express = require('express')
 const _ = require('lodash')
 const morgan = require('morgan')
 const PORT = 3000
+
+// Get the client
+const mysql = require('mysql2/promise')
+
+// Create connection to the database
+const connection = mysql.createConnection({
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_NAME,
+})
 
 // Create a new Express app
 const app = express()
@@ -17,12 +30,56 @@ app.use(express.json())
 // Log information about all incoming requests using morgan
 app.use(morgan('dev'))
 
-// GET /
+/**
+ * GET
+ */
+
 app.get('/', (req, res) => {
 	res.send({
 		message: "What's up bitch!",
 	})
 })
+
+/**
+ * GET /URL
+ */
+
+const appGet = database => {
+	app.get(`/${database}`, async (req, res) => {
+		const db = await connection
+		const [rows] = await db.query(`SELECT * FROM ${database}`)
+		res.send(rows)
+	})
+}
+
+appGet('movies')
+appGet('directors')
+appGet('director_movie')
+
+/**
+ * GET /URL/:id
+ */
+
+const appGetId = database => {
+	app.get(`/${database}/:id`, async (req, res) => {
+		const { id } = req.params
+		const db = await connection
+		const [rows] = await db.query(`SELECT * FROM ${database} WHERE id="${id}"`)
+
+		if (!rows.length) {
+			res.status(404).send({
+				message: `Sorry, there is no record of id '${id}'.`
+			})
+			return
+		}
+
+		res.send(rows[0])
+	})
+}
+
+appGetId('movies')
+appGetId('directors')
+appGetId('director_movie')
 
 // Catch requests where a route does not exist
 app.use((req, res) => {
@@ -33,5 +90,5 @@ app.use((req, res) => {
 
 // Start listening for incoming requests on port 3000
 app.listen(PORT, () => {
-	console.log(`Server started on localhost:${PORT}`)
+	console.log(`YEY! Server started on localhost:${PORT}`)
 })
