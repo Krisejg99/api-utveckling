c4fb31c6433d040792e8699318378aceeceb1f01
Kristopher Gray <kristopher.gray99@gmail.com>
2023-01-20 15:37:49 +0100

    get users and phones in different ways

--- 04-prisma/script.ts
+++ 04-prisma/script.ts
@@ -6,9 +6,75 @@ const main = async () => {
 	// Write Prisma Client queries here
 	console.log('It works?')
 
-	// Get all users and console.log them
-	const users = await prisma.users.findMany()
-	console.log(users)
+	// // Get all phones
+	// const phones = await prisma.phones.findMany({
+	// 	select: {
+	// 		manufacturer: true,
+	// 		model: true,
+	// 	},
+	// 	where: {
+	// 		manufacturer: "Apple",
+	// 	},
+	// })
+	// console.log('Phones:', phones)
+
+	// // Get all users (maybe filter a bit if you want :) )
+	// const users = await prisma.users.findMany({
+	// 	where: {
+	// 		name: {
+	// 			contains: 'an'
+	// 		}
+	// 	},
+	// 	orderBy: {
+	// 		name: 'asc'
+	// 	},
+	// 	take: 2,
+	// 	skip: 1
+	// })
+	// console.log('Users:', users)
+
+	// // Get one user
+	// const user = await prisma.users.findFirst({
+	// 	where: {
+	// 		name: "Korben Dallas",
+	// 	}
+	// })
+	// console.log('User:', user)
+
+	// // Get a specific user
+	// const user = await prisma.users.findFirst({
+	// 	where: {
+	// 		id: 5,
+	// 	}
+	// })
+	// console.log('Unique user:', user)
+
+	// // Get a specific user and their phone
+	// const user = await prisma.users.findUnique({
+	// 	where: {
+	// 		id: 4
+	// 	},
+	// 	include: {
+	// 		phones: true
+	// 	}
+	// })
+	// console.log('User:', user)
+
+	// // Get all users and their phones
+	// const users = await prisma.users.findMany({
+	// 	include: {
+	// 		phones: true
+	// 	}
+	// })
+	// console.dir(users, { depth: null})
+
+	// Get all phones and thir users (if they have any)
+	const phones = await prisma.phones.findMany({
+		include: {
+			users: true
+		}
+	})
+	console.dir(phones, { depth: null})
 }
 
 main()
