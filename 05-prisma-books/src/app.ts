023640d0ac55ec16d72abb5905f3cc8fc3c41dc4
Kristopher Gray <kristopher.gray99@gmail.com>
2023-01-31 10:06:41 +0100

    npm install cors

--- 05-prisma-books/src/app.ts
+++ 05-prisma-books/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import morgan from "morgan"
 import routes from "./routes/index"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use(routes)
 
