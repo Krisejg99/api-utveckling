c3cdefc7486eba60712ff327a7a636108c02986d
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-21 11:18:17 +0100

    connect and disconnect to Socket.IO server

--- 07-chat/frontend/src/main.ts
+++ 07-chat/frontend/src/main.ts
@@ -1,5 +1,23 @@
 import './assets/scss/style.scss'
+import { io, Socket } from 'socket.io-client'
+
+const SOCKED_HOST = import.meta.env.VITE_APP_SOCKET_HOST
 
 const messageEl = document.querySelector('#message') as HTMLInputElement
 const messageFormEl = document.querySelector('#message-form') as HTMLFormElement
 const messagesEl = document.querySelector('#messages') as HTMLDivElement
+
+
+// Connect to Socket.IO server
+const socket = io(SOCKED_HOST)
+
+
+// Listen for when connection is established
+socket.on('connect', () => {
+	console.log('CONNECTED TO SERVER!!!')
+})
+
+// Listen for when the server disocnnects
+socket.on('disconnect', () => {
+	console.log('DISCONNECTED FROM SERVER... :(')
+})
